d4406309757566849e91b3879336ff89ddf1bbbd
Andrzej <andrzej.popek@ec2.pl>
2021-04-26 20:14:28 +0200

    Add debounced search input

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { TextField } from "@material-ui/core";
+import React, { useCallback, useMemo, useState } from "react";
+import debounce from "lodash.debounce";
 
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  const debouncedSearch = useMemo(
+    () =>
+      debounce((inputValue) => {
+        setDebouncedValue(inputValue);
+      }, 1000),
+    [setDebouncedValue]
+  );
+
+  const handleChange = useCallback(
+    (e) => {
+      setSearchValue(e.target.value);
+      debouncedSearch(e.target.value);
+    },
+    [debouncedSearch]
+  );
+
   return (
     <>
-      <h1>Search</h1>
+      <form noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Search for gifs" onChange={handleChange} value={searchValue} />
+      </form>
+      <p>{debouncedValue}</p>
     </>
   );
 };
